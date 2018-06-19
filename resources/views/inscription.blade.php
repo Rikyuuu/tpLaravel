5555810d6bc494fe405c8e4e6dfbbf59602a2889
Rikyuuu <julien.delissenne@gmail.com>
2018-06-19 23:36:44 +0200

    Chapitre suivant : Personnaliser nos messages d'erreur. Modification de la vue layaout et du formulaire d'inscription, ajout d'un cdn Bulma sur les vues.

--- resources/views/inscription.blade.php
+++ resources/views/inscription.blade.php
@@ -1,23 +1,45 @@
 @extends('layout')
 
 @section('contenu')
-    <form action="/inscription" method="POST">
+    <form action="/inscription" method="POST" class="section">
         {{ csrf_field() }}
 
-        <p><input type="email" name="email" placeholder="Email" value="{{ old('email') }}"></p>
-        @if($errors->has('email'))
-            <p>{{ $errors->first('email') }}</p>
-        @endif
 
-        <p><input type="password" name="password" placeholder="Mot de passe"></p>
-        @if($errors->has('password'))
-            <p>{{ $errors->first('password') }}</p>
-        @endif
+        <div class="field">
+            <label class="label">Adresse e-mail</label>
+            <div class="control">
+                <input class="input" type="email" name="email" value="{{ old('email') }}">
+            </div>
+            @if($errors->has('email'))
+                <p class="help is-danger">{{ $errors->first('email') }}</p>
+            @endif
+        </div>
 
-        <p><input type="password" name="password_confirmation" placeholder="Confirmation du mot de passe"></p>
-        @if($errors->has('password_confirmation'))
-            <p>{{ $errors->first('password_confirmation') }}</p>
-        @endif
-        <p><input type="submit" value="M'inscrire"></p>
+        <div class="field">
+            <label class="label">Mot de passe</label>
+            <div class="control">
+                <input class="input" type="password" name="password">
+            </div>
+            @if($errors->has('password'))
+                <p class="help is-danger">{{ $errors->first('password') }}</p>
+            @endif
+        </div>
+
+        <div class="field">
+            <label class="label">Mot de passe (Confirmation)</label>
+            <div class="control">
+                <input class="input" type="password" name="password_confirmation">
+            </div>
+            @if($errors->has('password'))
+                <p class="help is-danger">{{ $errors->first('password_confirmation') }}</p>
+            @endif
+        </div>
+
+        <div class="field">
+            <div class="control">
+                <button class="button is-link" type="submit">M'inscrire</button>
+            </div>
+        </div>
+        
     </form>
 @endsection
